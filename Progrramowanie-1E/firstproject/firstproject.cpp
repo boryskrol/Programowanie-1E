626f7db79f3512fe6040d7151be0d37d90c7394c
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-09-20 08:53:53 +0200

    koniec lekcji 20.09.2022

---
+++ Progrramowanie-1E/firstproject/firstproject.cpp
@@ -0,0 +1,13 @@
+// firstproject.cpp : This file contains the 'main' function. 
+// Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hel\nlo World!\n";
+	std::cout << "Hello World!\n";
+
+	std::cout << "Predkosc to km\\h";
+}
