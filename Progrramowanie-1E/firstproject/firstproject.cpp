69c6f1269bb2ac11ea18971c9a8e76b4c07e4a44
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-09-21 09:54:22 +0200

    21.09.22

--- Progrramowanie-1E/firstproject/firstproject.cpp
+++ Progrramowanie-1E/firstproject/firstproject.cpp
@@ -1,13 +1,41 @@
-// firstproject.cpp : This file contains the 'main' function. 
-// Program execution begins and ends there.
-//
+// FirstProject.cpp : This file contains the 'main' function.
+//  Program execution begins and ends there.
+
 
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hel\nlo World!\n";
-	std::cout << "Hello World!\n";
+    /*
+   Instrukcja do wyœwietlania informacji na standardowy strumieñ wyjœcia.
+   Domyœlnie, jesli nie zosta³o to zmienione, jest to konsola w której aplikacja
+   zosta³a uruchomiona.
+   Aby wyswietliæ na konsoli nalezy u¿yæ std::cout.
+   Na konosle wywy³amy za pomoca operatora <<.
+   Po nim musi znajdowaæ siê dana do wys³ania.
+   Instrukcjê koñczymy znakiem œrednik.
+   "Hello World!\n" - jest to sta³a o wartoœci tekstowej.
+   Sta³e tekstowe s¹ ograniczone znakami cudzys³owu.
+   * ' (apostrof) - jesli jest jeden znak
+   * " (cudzys³ów) - jesli jest zero lub wiêcej znaków
+   W sta³ych tekstowych moga byæ wstawiane znaki specjalne. Takie znaki
+   zaczynaj¹ siê od znaku \ (backslash) po którym mo¿e siê znajdowaæ:
+   * jedna litera alfabetu angielskiego
+   * trzy cyfry daj¹ce liczbê ósemkow¹
+   Przyk³adowo:
+   * \n - symbol oznaczaj¹cy now¹ liniê.
+   * \t - symbol oznaczaj¹cy znak tabulatora
+   * \\ - symbol oznaczaj¹cy znak \
+   * \' - symbol oznaczaj¹cy znak '
+   * \" - symbol oznaczaj¹cy znak "
+   * \141 - symbol ósemkowy oznaczajacy zgodnie z tablic¹ kodów ASCII znak m³aej literki a
+   */
+    std::cout << "Hel\nlo World!\n";
+    std::cout << "Hello World!\n";
+ 
+    std::cout << " - On mi drink daje i chce robic ze mna jakies dziwne akcje\n" << " - A ile ty masz lat?\n" << - 17;
+std::cout << "\n";
 
-	std::cout << "Predkosc to km\\h";
+std::cout << "\n";
+
 }
