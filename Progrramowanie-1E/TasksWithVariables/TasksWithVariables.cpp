f4079938909cdb7e6b0dc06c66461c2cffde3ed9
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-10-04 09:04:45 +0200

    04.10.22

--- Progrramowanie-1E/TasksWithVariables/TasksWithVariables.cpp
+++ Progrramowanie-1E/TasksWithVariables/TasksWithVariables.cpp
@@ -1,20 +1,37 @@
-// TasksWithVariables.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+/*
+Napisz funkcjê która pobierze dwie liczby oraz wykona podstawowe operacje arytmetyczne
+*/
+
+
+void task1()
+{
+    int firstNumber, secondNumber;
+    std::cout << "Podaj pierwsza liczbe:\n";
+    std::cin >> firstNumber;
+
+    std::cout << "Podaj druga liczbe:\n";
+    std::cin >> secondNumber;
+
+    //suma    ró¿nica    iloczyn   iloraz   reszta
+    int sum, difference, product, quotient, rest;
+
+    sum = firstNumber + secondNumber;
+    difference = firstNumber - secondNumber; 
+    product = firstNumber * secondNumber;
+    quotient = firstNumber / secondNumber;
+    rest = firstNumber % secondNumber;
+
+    std::cout << "Suma: " << sum << "\n";
+    std::cout << "Roznica: " << difference << "\n";
+    std::cout << "Iloczyn: " << product << "\n";
+    std::cout << "Iloraz: " << quotient << "\n";
+    std::cout << "Reszta: " << rest << "\n";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    task1();
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
